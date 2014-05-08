07b29204641cba289fb5998cdbcbfa18ea938b75
Shilpa Agrawal <shilpamagrawal@Shilpas-MacBook-Air.local>
2014-05-08 01:02:47 -0400

    kdsfjaldksf

--- about_page/about_page.php
+++ about_page/about_page.php
@@ -4,7 +4,7 @@
    <head> 
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <link type='text/css' rel='stylesheet' href="program_notfound.css" />
+    <link type='text/css' rel='stylesheet' href="about_page.css" />
     <link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
     <link type="text/css" href="../nav_bar_header/nav_bar.css" rel="stylesheet" />
     <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
@@ -16,9 +16,9 @@
 </body>
 <body style="background-color: #F0F0F0; color: #003366;">
   <div class="mybody">
-    <div id="oops_well" class="well well-lg">
-        <h1 id="oops">Oops...</h1>
-        <p>Looks like we couldn't find the program you were looking for. </p>
+    <div id="about_well" class="well well-lg">
+        <h1 id="about">Under Construction...</h1>
+        <p>The About Page is in the works... Stay Tuned!</p>
         <p><a href="../user_profile/user_profile.php" id="home_button" class="btn btn-primary btn-lg" style="color: #003366;">Go Back To Your Home Page</a></p>
     </div>
   </div>
